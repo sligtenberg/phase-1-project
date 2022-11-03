066e5ddc0d9b3cc9759bcfa2aa3cd9b1bc86a251
stephen ligtenberg <bountyhunter24@gmail.com>
2022-11-02 23:10:34 -0400

    function restructuring, db.json built, initial fetch, populating function

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
 document.addEventListener('DOMContentLoaded', () => {
     populateSnacks();
-    eventListeners();
+
+    // listen for insert money buttons event
+    for (let button of document.getElementById('add-money').children) {
+        button.addEventListener('click', () => {
+            handleMoneyInstertion(button.value)
+            updateAmtTenderedElement()
+        })
+    }
+
+    // listen for return money button event
+    document.getElementById('amt-tendered').children[1].addEventListener('click', () => {
+        handleReturn()
+        updateAmtTenderedElement()
+    })
 })
 
-// this function will populate the snacks from the server
+// this function populates the snacks from the server
 const populateSnacks = () => {
     fetch('http://localhost:3000/snacks')
     .then(response => response.json())
-    .then(snackCollObj => {
-        console.log(snackCollObj)
+    .then(snackCollection => {
         let table = document.getElementById('snack-display')
+        // iterate through the cells of the display case
+        // add the snacks from the server to the case
         for (let i = 0; i < table.rows.length; i++) {
             for (let j = 0; j < table.rows[i].cells.length; j++) {
-                if (snackCollObj[i * table.rows[0].cells.length + j] === undefined) break
+                let currentSnack = snackCollection[i * table.rows[0].cells.length + j]
+                let dispalyWindow = table.rows[i].cells[j]
+                if (currentSnack != undefined) {
+                    dispalyWindow.innerHTML = `
+                        <button type="button" class="snack-btn">
+                            ${currentSnack.name}<br>
+                            $${currentSnack.price.toFixed(2)}<br>
+                            ${currentSnack.quantity} left
+                        </button>
+                    `
+                    dispalyWindow.children[0].addEventListener('click', () => handleSnackOrder(currentSnack))
+                } 
                 else {
-                    console.log(i * table.rows[0].cells.length + j)
-                    console.log(table.rows[i].cells[j])
-                    table.rows[i].cells[j].textContent = snackCollObj[i * table.rows[0].cells.length + j].name    
+                    dispalyWindow.innerHTML = `<button type="button" disabled>Add Snack!</button>`
                 }
             }
         }
     })
+}
+
+// this function will handle when a user tries to order a snack
+const handleSnackOrder = (snack) => snack.quantity === 0 ?
+    alert(`Stevo's Snack Sampler is out of ${snack.name}!`) : totalTendered() < snack.price ?
+        alert(`You need to enter more money to purchase ${snack.name}!`) : snackDelivery(snack)
+
+// this function executes when a snack is to be delivered to the customer
+const snackDelivery = (snack) => {
+    snack.quantity--
+    fetch(`http://localhost:3000/snacks/${snack.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(snack)
+    })
+    .then(res => res.json)
+    .then(populateSnacks())
 }
 
 // this object represents money that has been inserted into the machine
@@ -30,22 +72,6 @@ const tenderedMoney = {
     quarters: {quantity: 0, value: 0.25},
     dimes: {quantity: 0, value: 0.1},
     nickels: {quantity: 0, value: 0.05},
-}
-
-const eventListeners = () => {
-    // insert money buttons
-    for (let button of document.getElementById('add-money').children) {
-        button.addEventListener('click', () => {
-            handleMoneyInstertion(button.value)
-            updateAmtTenderedElement()
-        })
-    }
-
-    // return money button
-    document.getElementById('amt-tendered').children[1].addEventListener('click', () => {
-        handleReturn()
-        updateAmtTenderedElement()
-    })
 }
 
 // this function adds money to the tenderedMoney object
