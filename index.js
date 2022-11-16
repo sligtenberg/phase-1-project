d307bf54888a126e20a66826ae349f4c4dfef949
stephen ligtenberg <bountyhunter24@gmail.com>
2022-11-16 02:52:57 -0500

    overal organization and commentary

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    populateSnackDisplay();
+
+    // these functions run BEFORE USER INPUT
+    // both add event listeners which vary based on data on the server
+    populateSnackDisplay()
+    populateCashDrawer()
+
+    // toggle customer mode and maintenance mode
+    // listen for radio button change
+    document.getElementById('role-toggle').addEventListener('click', () => {
+        applyRole()
+        updateDispenser()
+        populateSnackDisplay()
+    })
+
+    // CUSTOMER MODE EVENT LISTENERS:
 
     // listen for insert money button events
     for (let button of document.getElementById('add-money').children[1].children) {
@@ -12,25 +26,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // listen for return money button event
     document.getElementById('return-money').addEventListener('click', () => {
-        updateDispenser(moneyBoxToString(tenderedMoney.money))
+        updateDispenser(describeChange(tenderedMoney.money))
         tenderedMoney.reset()
         updateAmtTendered(tenderedMoney.total())
     })
 
-    // listen for radio button change
-    document.getElementById('role-toggle').addEventListener('click', () => {
-        applyRole()
-        updateDispenser()
-    })
+    // MAINTENANCE MODE EVENT LISTENERS:
 
     //listen for cash drawer form submit
-    document.getElementById('cash-drawer').children[4].children[0].addEventListener('submit', (event) => {
+    document.getElementById('cash-drawer').children[4].children[0].addEventListener('submit', event => {
         event.preventDefault()
-        addMoneyToMachine(event.target)
+        handleCashDrawerSubmit(event.target)
     })
 
     // listen for auto reset
-    document.getElementById('cash-drawer').children[1].addEventListener('click', autoReset)
+    document.getElementById('cash-drawer').children[1].addEventListener('click', () => {
+        let tblRows = document.getElementById('cash-drawer').children[4].children[1].children
+        for (let i = 0; i < defaultCashDrawer.length; i++) {
+            tblRows[i+1].children[2].children[0].value = defaultCashDrawer[i] - tblRows[i+1].children[1].textContent
+        }
+    })
 
     // listen for cancel btn
     document.getElementById('cash-drawer').children[2].addEventListener('click', () => {
@@ -38,71 +53,11 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 })
 
+// GLOBAL VARIABLES:
+
 // this is the default quantity of each denomination of change that the maintenance person should leave in the machine
 // when autoRest is called, the fields to add money are automatically filled to set the money in the macihine to these numbers
 const defaultCashDrawer = [0, 50, 200, 500, 500]
-
-// this function resets the quantity of each denomination of change in the machine to a default level
-const autoReset = () => {
-    let tblRows = document.getElementById('cash-drawer').children[4].children[1].children
-    for (let i = 0; i < defaultCashDrawer.length; i++) {
-        tblRows[i+1].children[2].children[0].value = defaultCashDrawer[i] - tblRows[i+1].children[1].textContent
-    }
-}
-
-// this function will take a snack as an argument, and update that snack on the server
-const updateSnackOnServer = (snack) => {
-    fetch(`http://localhost:3000/snacks/${snack.id}`, {
-        method: 'PATCH',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(snack)
-    })
-}
-
-// this function handles the maintenance action of adding money to the machine
-const addMoneyToMachine = (submittedMoney) => {
-    fetch('http://localhost:3000/cash')
-    .then(res => res.json())
-    .then(moneyInCashDrawer => {
-        for (let i = 0; i < moneyInCashDrawer.length; i++) {
-            if (submittedMoney[i + 1].value === '') submittedMoney[i + 1].value = 0
-            moneyInCashDrawer[i].quantity += Number(submittedMoney[i + 1].value)
-            document.getElementById('cash-drawer').children[4].children[1].children[i + 1].children[1].textContent = moneyInCashDrawer[i].quantity
-        }
-        sendMoneyInCashDrawer(moneyInCashDrawer)
-        document.getElementById('cash-drawer').children[4].children[0].reset()
-    })
-}
-
-// this function switches us to the correct mode
-const applyRole = () => {
-    const customerClass = document.getElementsByClassName('customer')
-    const maintenanceClass = document.getElementsByClassName('maintenance')
-    if (document.getElementById('role-toggle').children[0].checked) {
-        for (let item of maintenanceClass) {
-            item.disabled = true
-            item.style.display = "none"
-        }
-        for (let item of customerClass) {
-            item.disabled = false
-            item.style.display = "block"
-        }
-    }
-    else {
-        for (let item of maintenanceClass) {
-            item.disabled = false
-            item.style.display = "block"
-        }
-        for (let item of customerClass) {
-            item.disabled = true
-            item.style.display = "none"
-        }
-        document.getElementById('add-money').style.display = "none"
-        document.getElementById('cash-drawer').style.display = "block"
-        document.getElementById('dispenser').style.display = "none"
-        populateCashDrawer()
-    }
-}
 
 // this object represents money that has been inserted into the machine
 const tenderedMoney = {
@@ -126,31 +81,18 @@ const tenderedMoney = {
     }
 }
 
-// this function takes a money array and returns a string describing it
-const moneyBoxToString = (moneyArray) => {
-    const returnSentence = []
-    for (let denomination of moneyArray) {
-        switch (denomination.quantity) {
-            case 0: break
-            case 1: {
-                returnSentence.push(` ${denomination.quantity} ${denomination.name}`)
-                break
-            }
-            default: returnSentence.push(` ${denomination.quantity} ${denomination.name}s`)
-        }
-    }
-    return returnSentence
+// FUNCTIONS CALLED ON PAGE LOAD:
+
+// populateSnackDisplay() fetches all the snacks from the server and sends them to the displaySnack functions
+const populateSnackDisplay = () => {
+    fetch('http://localhost:3000/snacks')
+    .then(res => res.json())
+    .then(snackCollection => {
+        snackCollection.forEach(displaySnack)
+    })
 }
 
-// this function updates the HTML element to reflect the amount of money the user has entered
-// should be called: 1) when money is entered, 2) when something is purshased, 3) when money is returned
-const updateAmtTendered = (amount) => document.getElementById('amt-tendered').textContent = `$${amount.toFixed(2)}`
-
-// this function writes to the dispenser
-// it will be called for three cases: 1) money sent to user, 2) snack sent to user, 3) error message sent to user
-const updateDispenser = (content) => document.getElementById('dispenser').children[1].textContent = content
-
-// this function fetches data from db.json and uses it to populate the cash drawer
+// populateCashDrawer fetches data from the server and uses it to populate the cash drawer
 const populateCashDrawer = () => {
     fetch('http://localhost:3000/cash')
     .then(res => res.json())
@@ -162,18 +104,33 @@ const populateCashDrawer = () => {
     })
 }
 
-// this function gets the snacks from the server
-const populateSnackDisplay = () => {
-    fetch('http://localhost:3000/snacks')
-    .then(res => res.json())
-    .then(snackCollection => {
-        snackCollection.forEach(displaySnack)
+// TOOL FUNCTIONS:
+
+// updateSnackOnServer takes a snack object as an argument, and update that snack on the server via PATCH request
+const updateSnackOnServer = snack => {
+    fetch(`http://localhost:3000/snacks/${snack.id}`, {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(snack)
     })
 }
 
-// this function takes a snack and adds it to the proper place in the snack display
-// the proper place is dictated by the snack id
-const displaySnack = (snack) => {
+// sendMoneyInCashDrawer takes an array of money objects and send them to the server via a PATCH request
+const sendMoneyInCashDrawer = moneyToSend => {
+    for (let i = 0; i < moneyToSend.length; i++) {
+        fetch(`http://localhost:3000/cash/${i+1}`, {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(moneyToSend[i])
+        })
+        for (let i = 0; i < 100000000; i++) {} // dummy loop to delay the fetch requests
+    }
+}
+
+// displaySnack takes a snack object as an arg and adds it to the proper place in the snack display - dictated by the snack id
+// displaySnack builds the html element for the snack on both the customer side and the maintenance side
+// display snack adds event listeners to the snacks
+const displaySnack = snack => {
     const table = document.getElementById('display').children[1]
     let tableElement = table.rows[Math.floor((snack.id - 1) / table.rows[0].cells.length)].cells[(snack.id - 1) % table.rows[0].cells.length]
     tableElement.innerHTML = `
@@ -191,83 +148,139 @@ const displaySnack = (snack) => {
     `
     applyRole()
     tableElement.children[0].addEventListener('click', () => handleSnackOrder(snack))
-    tableElement.children[1].addEventListener('submit', event => handleSnackEdit(event, snack))
+    tableElement.children[1].addEventListener('submit', event => handleSnackEditSubmit(event, snack))
 }
 
-// this function takes edits made to a snack (form submission) and a snack as args, and updates the snack on the server
-// called as a result of a submit event
-// need to add ability to prevent bad submissions (negative quantities, prices, non-numbers, etc.)
-const handleSnackEdit = (event, snack) => {
-    event.preventDefault()
-    const newName = event.target[0].value
-    const newPrice = Number(event.target[1].value)
-    const newQuantity = Number(event.target[2].value)
-    snack.name = newName
-    snack.price = newPrice
-    snack.quantity = newQuantity
-    updateSnackOnServer(snack)
-}
-
-// this function will handle when a user tries to order a snack
-const handleSnackOrder = (snack) => {
-    if (snack.quantity === 0) updateDispenser(`Stevo's Snack Sampler is out of ${snack.name}`)
-    else if (tenderedMoney.total() < snack.price) updateDispenser(`You need to enter more money to purchase ${snack.name}`)
-    else purchaseSnack(snack)
-}
-
-// this function takes a snack and tries to make change
-// if it can make change:
-//  - deliver the snack
-//  - update the amount tendered
-// if it cannot make change: alert the user that change cannot be made for thier order
-const purchaseSnack = async (snack) => {
-    fetch('http://localhost:3000/cash')
-    .then(res => res.json())
-    .then(moneyInCashDrawer => {
-        let changeNeeded = tenderedMoney.total() - snack.price
-        const potentialChange = [0, 0, 0, 0, 0]
-        for (let i = 0; i < moneyInCashDrawer.length; i++) {
-            moneyInCashDrawer[i].quantity += tenderedMoney.money[i].quantity
-            while (changeNeeded >= moneyInCashDrawer[i].value && moneyInCashDrawer[i].quantity > 0) {
-                potentialChange[i]++
-                moneyInCashDrawer[i].quantity--
-                changeNeeded = (changeNeeded - moneyInCashDrawer[i].value).toFixed(2)
+// describeChange takes a money array and returns an array which better desbribes it
+const describeChange = moneyArray => {
+    const returnSentence = []
+    for (let denomination of moneyArray) {
+        switch (denomination.quantity) {
+            case 0: break
+            case 1: {
+                returnSentence.push(` ${denomination.quantity} ${denomination.name}`)
+                break
             }
+            default: returnSentence.push(` ${denomination.quantity} ${denomination.name}s`)
         }
-        if (Number(changeNeeded) === 0) {
-            for (let i = 0; i < potentialChange.length; i++) {
-                tenderedMoney.money[i].quantity = potentialChange[i]
-            }
-            sendMoneyInCashDrawer(moneyInCashDrawer)
-            updateAmtTendered(tenderedMoney.total())
-            snackDelivery(snack)
-        }
-        else {
-            // add functionality to try again, but getting around the quarters and dimes problem
-            updateDispenser(`Stevo's Snack Sampler can't make change for ${snack.name}`)
-        }
-    })
-}
-
-async function sendMoneyInCashDrawer(moneyToSend) {
-    for (let i = 0; i < moneyToSend.length; i++) {
-        await fetch(`http://localhost:3000/cash/${i+1}`, {
-            method: 'PATCH',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(moneyToSend[i])
-        })
-        for (let i = 0; i < 100000000; i++) {} // dummy loop to delay the fetch requests
     }
+    return returnSentence
 }
+
+// updateDispenser takes content as an arg writes it in the dispenser area for the user to view
+// it will be called for three cases: 1) money sent to user, 2) snack sent to user, 3) error message sent to user
+// also called with no arg in order to delete the content in the dispenser area
+const updateDispenser = content => document.getElementById('dispenser').children[1].textContent = content
+
+// updateAmtTendered updates the 'amt-tendered' HTML element to reflect the amount of money the user has entered
+// should be called: 1) when money is entered, 2) when something is purshased, 3) when money is returned
+const updateAmtTendered = amount => document.getElementById('amt-tendered').textContent = `$${amount.toFixed(2)}`
 
 // this function executes when a snack should be delivered to the customer
 // decrease the quantity of this particular snack
 // send it to the display case
 // send the updated snack back to the server 
 // update the html element
-const snackDelivery = (snack) => {
+const snackDelivery = snack => {
     snack.quantity--
     updateDispenser(`${snack.name}!`)
     updateSnackOnServer(snack)
     displaySnack(snack)
+}
+
+// MODE TOGGLE:
+
+// applyRole toggles between customer and maintenance mode
+const applyRole = () => {
+    const customerClass = document.getElementsByClassName('customer')
+    const maintenanceClass = document.getElementsByClassName('maintenance')
+    if (document.getElementById('role-toggle').children[0].checked) {
+        for (let item of maintenanceClass) {
+            item.disabled = true
+            item.style.display = "none"
+        }
+        for (let item of customerClass) {
+            item.disabled = false
+            item.style.display = "block"
+        }
+    }
+    else {
+        for (let item of maintenanceClass) {
+            item.disabled = false
+            item.style.display = "block"
+        }
+        for (let item of customerClass) {
+            item.disabled = true
+            item.style.display = "none"
+        }
+    }
+}
+
+// FUNCTIONS CALLED BY CUSTOMER MODE BUTTONS:
+
+// handleSnackOrder is called when a user clicks a snack button
+// 1) checks if there are any left
+// 2) chacks enough money has been inserted
+// 3) checks if change can be made
+// 4) sends the snack to the customer, updates the server, and updates the amount tendered
+const handleSnackOrder = snack => {
+    if (snack.quantity === 0) updateDispenser(`Stevo's Snack Sampler is out of ${snack.name}`)
+    else if (tenderedMoney.total() < snack.price) updateDispenser(`You need to enter more money to purchase ${snack.name}`)
+    else {
+        fetch('http://localhost:3000/cash')
+        .then(res => res.json())
+        .then(moneyInCashDrawer => {
+            let changeNeeded = tenderedMoney.total() - snack.price
+            const potentialChange = [0, 0, 0, 0, 0]
+            for (let i = 0; i < moneyInCashDrawer.length; i++) {
+                moneyInCashDrawer[i].quantity += tenderedMoney.money[i].quantity
+                while (changeNeeded >= moneyInCashDrawer[i].value && moneyInCashDrawer[i].quantity > 0) {
+                    potentialChange[i]++
+                    moneyInCashDrawer[i].quantity--
+                    changeNeeded = (changeNeeded - moneyInCashDrawer[i].value).toFixed(2)
+                }
+            }
+            if (Number(changeNeeded) === 0) {
+                for (let i = 0; i < potentialChange.length; i++) {
+                    tenderedMoney.money[i].quantity = potentialChange[i]
+                }
+                sendMoneyInCashDrawer(moneyInCashDrawer)
+                updateAmtTendered(tenderedMoney.total())
+                snackDelivery(snack)
+            }
+            else {
+                // add functionality to try again, but getting around the quarters and dimes problem
+                updateDispenser(`Stevo's Snack Sampler can't make change for ${snack.name}`)
+            }
+        })
+    
+    }
+}
+
+// FUNCTIONS CALLED BY MAINTENACE MODE BUTTONS:
+
+// handleCashDrawerSubmit takes the quantities added by the user as an arg and updates the server with that data
+const handleCashDrawerSubmit = submittedMoney => {
+    // first, we need to get the money already on the server
+    fetch('http://localhost:3000/cash')
+    .then(res => res.json())
+    .then(moneyInCashDrawer => {
+        for (let i = 0; i < moneyInCashDrawer.length; i++) {
+            moneyInCashDrawer[i].quantity += Number(submittedMoney[i + 1].value) // add the quantities added by the user
+            document.getElementById('cash-drawer').children[4].children[1].children[i + 1].children[1].textContent = moneyInCashDrawer[i].quantity
+        }
+        sendMoneyInCashDrawer(moneyInCashDrawer)
+        document.getElementById('cash-drawer').children[4].children[0].reset()
+    })
+}
+
+// handleSnackEditSubmit takes edits made to a snack (form submission) and a snack as args, and updates the snack on the server
+// called as a result of a submit event
+// need to add ability to prevent bad submissions (negative quantities, prices, non-numbers, etc.)
+const handleSnackEditSubmit = (event, snack) => {
+    event.preventDefault()
+    snack.name = event.target[0].value
+    snack.price = Number(event.target[1].value)
+    snack.quantity = Number(event.target[2].value)
+    updateSnackOnServer(snack)
 }
